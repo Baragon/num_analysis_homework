d5927629078169ca1249052e10025bf0d6cd80d7
Baragon <baragon92@gmail.com>
2013-09-09 19:38:13 +0400

    added IFunction interface

---
+++ src/nikitin/numanalysis/task1/IFunction.java
@@ -0,0 +1,14 @@
+package nikitin.numanalysis.task1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Baragon
+ * Date: 09.09.13
+ * Time: 19:36
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IFunction {
+    public String toString();
+
+    public double Value();
+}
