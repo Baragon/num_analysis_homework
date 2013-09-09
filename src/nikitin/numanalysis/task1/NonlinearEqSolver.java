2b8d4bd41c3ee21de147c8039dd90d92fb18fc02
Baragon <baragon92@gmail.com>
2013-09-09 19:36:26 +0400

    Initial commit

---
+++ src/nikitin/numanalysis/task1/NonlinearEqSolver.java
@@ -0,0 +1,20 @@
+package nikitin.numanalysis.task1;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Baragon
+ * Date: 09.09.13
+ * Time: 19:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class NonlinearEqSolver {
+    public static void Main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintStream out = System.out;
+        out.println("Численные методы решения нелинейных алгебраических и трансцендентных уравнений");
+
+    }
+}
